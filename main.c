b64414ceba37faf148dc983fe31bc2837cc04eea
Olexiy Burov <alex.burov@outlook.com>
2016-04-27 16:23:06 -0700

    gpu_fft lib

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
-     #include <stdlib.h>
-     #include <unistd.h>
-     #include <stdio.h>
-     #include <math.h>
-     #include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include "mailbox.h"
+#include "gpu_fft.h"
+
+char Usage[] =
+        "Usage: hello_fft.bin log2_N [jobs [loops]]\n"
+                "log2_N = log2(FFT_length),       log2_N = 8...22\n"
+                "jobs   = transforms per batch,   jobs>0,        default 1\n"
+                "loops  = number of test repeats, loops>0,       default 1\n";
+
+unsigned Microseconds(void) {
+     struct timespec ts;
+     clock_gettime(CLOCK_REALTIME, &ts);
+     return ts.tv_sec*1000000 + ts.tv_nsec/1000;
+}
+
+int main(int argc, char *argv[]) {
+     struct GPU_FFT_COMPLEX *dataIn,*dataOut;
+     struct GPU_FFT *fftinfo;
+
+     mb = mbox_open();
+     gpu_fft_prepare(mb,12,GPU_FFT_FWD,1,&fftinfo);
+
+     dataIn = fftinfo->in;
+     dataOut = fftinfo->out;
+
+     usleep(1); // Yield to OS
+     t[0] = Microseconds();
+     gpu_fft_execute(fftinfo); // call one or many times
+     t[1] = Microseconds();
      
-     #include "mailbox.h"
-     #include "gpu_fft.h"
+     gpu_fft_release(fftinfo); // Videocore memory lost if not freed !
+     return 0;
+}
