f7447d7a91e811d6643313d1e616f819cd228dd7
Olexiy Burov <alex.burov@outlook.com>
2016-04-27 14:12:41 -0700

    gpu_fft lib

---
+++ main.c
@@ -0,0 +1,8 @@
+     #include <stdlib.h>
+     #include <unistd.h>
+     #include <stdio.h>
+     #include <math.h>
+     #include <time.h>
+     
+     #include "mailbox.h"
+     #include "gpu_fft.h"
